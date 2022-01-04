73f8e27c711f141c8175127856e70eb3122d07fc
Ben Kogan <brk57@cornell.edu>
2022-01-04 06:16:24 -0500

    adjusted height/width, game working

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,28 @@
+const div = document.getElementById("minesweeper") as HTMLDivElement;
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-const windowLength = 600;
-canvas.width = windowLength;
-canvas.height = windowLength;
+const width = div.clientWidth;
+const height = div.clientHeight;
 
-const step = 40;
-const rows = Math.floor(windowLength / step);
-const cols = Math.floor(windowLength / step);
+canvas.width = width;
+canvas.height = height;
 
-let bombCount: number = 50;
+ctx.font = "20px sans-serif";
+
+const cols = 30;
+const step = width / cols;
+const rows = Math.floor(height / step);
+//const rows = Math.floor(windowLength / step);
+//const cols = Math.floor(windowLength / step);
+
+function stringToPos(pos: string): number[] {
+  return pos.split("_").map((s) => parseInt(s));
+}
+
+function posToString([x, y]: number[]): string {
+  return `${x}_${y}`;
+}
 
 function getRandomPos(): number[] {
   return [Math.floor(Math.random() * cols), Math.floor(Math.random() * rows)];
@@ -59,21 +72,22 @@ interface Tile {
 }
 
 function drawGrid() {
+  ctx.lineWidth = 2;
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      ctx.strokeRect(i * step, j * step, step, step);
+      ctx.strokeRect(j * step, i * step, step, step);
     }
   }
   ctx.lineWidth = 5;
-  ctx.strokeRect(0, 0, windowLength, windowLength);
+  ctx.strokeRect(0, 0, width, height);
 }
 
-function randomSetup(bombs: number): Map<number[], Tile> {
-  const setup = new Map<number[], Tile>(); // top left coordinate to tile
+function randomSetup(bombs: number): Map<string, Tile> {
+  const setup = new Map<string, Tile>(); // top left coordinate to tile
   // Register tiles
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
-      setup.set([i, j], {
+      setup.set(`${j}_${i}`, {
         revealed: false,
         bomb: false,
         flagged: false,
@@ -81,19 +95,20 @@ function randomSetup(bombs: number): Map<number[], Tile> {
       });
     }
   }
+  console.log(setup);
   // Determine bombs and numbers
   for (let i = 0; i < bombs; i++) {
     let minePos = getRandomPos();
-    while ((setup.get(minePos) as Tile).bomb) {
+    while ((setup.get(posToString(minePos)) as Tile).bomb) {
       minePos = getRandomPos();
     }
-    const tile = setup.get(minePos) as Tile;
+    const tile = setup.get(posToString(minePos)) as Tile;
     for (let x = -1; x <= 1; x++) {
       for (let y = -1; y <= 1; y++) {
         if (x == 0 && y == 0) {
           tile.bomb = true;
         } else {
-          const offsetPos = [minePos[0] + x, minePos[1] + y];
+          const offsetPos = posToString([minePos[0] + x, minePos[1] + y]);
           if (setup.has(offsetPos)) {
             (setup.get(offsetPos) as Tile).num++;
           }
@@ -102,17 +117,39 @@ function randomSetup(bombs: number): Map<number[], Tile> {
     }
   }
 
-  // Reveal zero tiles
-  setup.forEach((tile, _) => {
-    if (!tile.bomb && tile.num == 0) {
-      tile.revealed = true;
-    }
-  });
-
   return setup;
 }
 
-let tiles: Map<number[], Tile> = randomSetup(bombCount);
+let firstClick: boolean = true;
+let bombCount: number = Math.floor((cols * rows) / 5);
+let tiles: Map<string, Tile> = randomSetup(bombCount);
+tiles.forEach((tile: Tile, _) => {
+  tile.revealed = true;
+});
+drawGame(false);
+
+// Reveals the tile at 'pos' and all of its neighbors. Repeats for all
+// neighbors that are also empty. Optionally pass in 'revealed' map for
+// all empty tiles that have already been revealed in this flood.
+function revealFlood(
+  [tileX, tileY]: number[],
+  revealed: Map<string, boolean> = new Map<string, boolean>()
+) {
+  revealed.set(posToString([tileX, tileY]), true);
+  for (let x = -1; x <= 1; x++) {
+    for (let y = -1; y <= 1; y++) {
+      const pos = [tileX + x, tileY + y];
+      if (tiles.has(posToString(pos))) {
+        const tile = tiles.get(posToString(pos)) as Tile;
+        if (tile.num == 0 && !revealed.has(posToString(pos))) {
+          revealFlood(pos, (revealed = revealed));
+        } else {
+          tile.revealed = true;
+        }
+      }
+    }
+  }
+}
 
 // Mouse Down Listener
 canvas.addEventListener("mousedown", (e: MouseEvent) => {
@@ -123,15 +160,15 @@ canvas.addEventListener("mousedown", (e: MouseEvent) => {
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
   const pos = [x / step, y / step].map(Math.floor);
-  if (!tiles.has(pos)) {
+  if (!tiles.has(posToString(pos))) {
     return;
   }
-  const tile = tiles.get(pos) as Tile;
+  const tile = tiles.get(posToString(pos)) as Tile;
   const alt = e.button == 2;
-  handleClick(tile, alt);
+  handleClick(tile, pos, alt);
 });
 
-function handleClick(tile: Tile, alt: boolean): void {
+function handleClick(tile: Tile, pos: number[], alt: boolean): void {
   if (tile.revealed) {
     return;
   }
@@ -139,13 +176,47 @@ function handleClick(tile: Tile, alt: boolean): void {
   let gameOver = false;
   if (tile.flagged) {
     tile.flagged = !tile.flagged;
-  } else if (!alt) {
+  } // handle hidden and unflagged tiles:
+  else if (!alt) {
     if (tile.bomb) {
       gameOver = true;
-    } else {
       tile.revealed = true;
+    } else {
+      if (tile.num == 0) {
+        revealFlood(pos);
+      } else {
+        tile.revealed = true;
+      }
     }
   } else {
     tile.flagged = true;
   }
+
+  drawGame(gameOver);
+}
+
+function handleFirstClick() {}
+
+function drawGame(gameOver: boolean) {
+  // Clear previous board and redraw grid
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawGrid();
+
+  if (gameOver) {
+    console.log("game over");
+  }
+
+  tiles.forEach((tile: Tile, pos: string) => {
+    const [x, y] = stringToPos(pos);
+    if (tile.revealed || tile.flagged) {
+      if (tile.flagged) {
+        ctx.fillText("F", step * x, step + step * y);
+      } else if (tile.bomb) {
+        ctx.fillText("B", step * x, step + step * y);
+      } else if (tile.num) {
+        const num = tile.num == 0 ? "" : `${tile.num}`;
+        ctx.fillText(num, step * x, step + step * y);
+      }
+    }
+  });
 }
